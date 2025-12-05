b57e53f8a10f259a198220112722bcf7e7340b2c
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2025-12-05 12:29:15 +0530

    added ignore file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Trash2, Edit2, Plus, Check, X } from "lucide-react";
 
 
-const API_URL = "http://localhost:7000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
   const [tasks, setTasks] = useState([]);
